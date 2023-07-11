6ab57238e22b0a228b9e381a6459913c502fa020
yatharth <yatharth.saini.int@paymeindia.in>
2023-07-11 15:21:41 +0530

    Added the login API too

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 
 # User Serializer
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,12 @@
-from .views import RegisterAPI
+import knox.views
+
+from .views import RegisterAPI, LoginAPI
 from django.urls import path
 
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name="login"),
+    path("api/logout/", knox.views.LogoutView.as_view(), name="logout"),
+    path("api/logoutall/", knox.views.LogoutAllView.as_view(), name="logoutall"),
 ]
 
