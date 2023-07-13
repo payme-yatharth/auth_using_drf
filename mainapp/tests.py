708041e4f77cd2342838c4668fd743c8cbefe1ef
payme-yatharth <138878377+yatharth-intern@users.noreply.github.com>
2023-07-13 17:39:23 +0530

    Update tests.py

---
+++ mainapp/tests.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from knox.models import AuthToken
+from rest_framework import status
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+        self.token = AuthToken.objects.create(user=self.user)[1]
+
+    def test_register_api(self):
+        url = '/api/register/'
+        data = {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'newpassword'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 2)
+        self.assertIn('user', response.data)
+        self.assertIn('token', response.data)
+
+    def test_login_api(self):
+        url = '/api/login/'
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_change_password_view(self):
+        url = '/api/change-password/'
+        data = {
+            'old_password': 'testpassword',
+            'new_password': 'newpassword'
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status'], 'success')
+        self.assertEqual(response.data['message'], 'Password updated successfully')
+
+    def test_change_password_view_unauthorized(self):
+        url = '/api/change-password/'
+        data = {
+            'old_password': 'testpassword',
+            'new_password': 'newpassword'
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
